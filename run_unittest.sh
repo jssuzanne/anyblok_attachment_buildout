f216bb76ea16c814c5c823f97f6c6fcc560440b0
jssuzanne <jssuzanne@anybox.fr>
2018-01-05 12:16:38 +0100

    Start to implement wkhtml2pdf

---
+++ run_unittest.sh
@@ -0,0 +1,2 @@
+./bin/nosetests anyblok_attachment/anyblok_attachment/tests/ -v -s --with-coverage --cover-package anyblok_attachment
+./bin/nosetests anyblok_attachment/anyblok_attachment/bloks --with-anyblok-bloks -v -s --with-coverage --cover-package=anyblok_attachment --anyblok-configfile=anyblok.cfg
